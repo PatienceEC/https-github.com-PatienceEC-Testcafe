9cefcdd8459e8551924c24c5029610f74c6c73ac
Patience <patiencekudyatasa@gmail.com>
2019-02-27 13:48:47 +0200

    New Test

---
+++ tests/Validate that when I click on Approval,I am re-directed to Approval page.js
@@ -0,0 +1,15 @@
+import { Selector }from 'testcafe';
+fixture `Dashboard Link`
+    .page `https://enabillstaging.saratoga.co.za`;
+    
+    const name = Selector('#UserName');
+    const pass = Selector('#Password');
+    
+test(`Approval`, async t => {
+    await t
+                .typeText(name,'patiencek@saratoga.co.za')
+                .typeText(pass,'3@5@yKk8PflBjaV8ZIeI')
+                .click(Selector('#LogOnSubmit'))
+                .click(Selector('#menuItems').find('a').nth(8).find('div'))
+    
+});
